8792f67fed7ea536a6dc2d14bd599985c13147ab
lleaff <lleaff@users.noreply.github.com>
2016-03-04 11:08:50 +0100

    free temporary maps and paths

--- src/freeing.c
+++ src/freeing.c
@@ -1,11 +1,22 @@
 #include "my.h"
 
+void free_char_grid(char **grid, int height)
+{
+    int y;
+
+    for (y = 0; y < height; y++)
+        free(grid[y]);
+    free(grid);
+}
+
 void free_map(t_map *map)
 {
-    noop(map);
+    free_char_grid(map->tab, map->h);
+    free(map);
 }
 
 void free_path(t_path *path)
 {
-    noop(path);
+    free(path->tab);
+    free(path);
 }
